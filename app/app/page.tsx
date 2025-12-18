f86090f4162bf8c26d7c1cbef1f00715859f9593
Purplehippo <oceanrabbit0101@protonmail.com>
2025-12-18 13:12:39 +0100

    changed document title to Forsk3d

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -3,15 +3,16 @@ import NavBar from "./components/NavBar";
 import LandingPage from "./components/LandingPage";
 import SecondPage from "./components/SecondPage";
 import Footer from "./components/Footer";
+import Head from "next/head";
 
 
 export default function Home() {
   return (
-    <div className="m-auto w-full h-full p-0 bg-emerald-400 font-sans flex-col gap-[2rem]">
-        <NavBar />
-        <LandingPage />
-        <SecondPage/>
-        <Footer/>
-    </div>
+      <div className="m-auto w-full h-full p-0 bg-emerald-400 font-sans  flex-col gap-[2rem]">
+          <NavBar />
+          <LandingPage />
+          <SecondPage/>
+          <Footer/>
+      </div>
   );
 }
