2d5d979c6b0f94c56bc47562a4c5a0d28882e31a
Purplehippo <oceanrabbit0101@protonmail.com>
2025-12-29 01:38:08 +0100

    made logo clickable in navbar

--- app/app/components/NavBar.tsx
+++ app/app/components/NavBar.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function NavBar() {
   return (
     <nav className="w-full py-4 px-8 bg-white shadow-md flex items-center gap-6 mb-[6rem]">
-      <div className="w-full flex items-center justify-center md:justify-start">
+      <a href="https://www.youtube.com/watch?v=UWCmqBB2UWg" target="_blank" className="w-full flex items-center justify-center md:justify-start">
         <Image
             className="w-auto h-auto"
             src="/logo.png"
@@ -14,7 +14,7 @@ export default function NavBar() {
             priority
           />
         <div className="text-xl font-bold text-gray-800">Forsk3D</div>
-      </div>
+      </a>
     </nav>
   );
 } 
